ea28ad81cb600744536e444c8ab20ad7aa3d5965
NickWang <hcfw007@126.com>
2018-07-26 12:14:46 +0800

    get categories of addons

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const request = require('request')
 const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
+const Curseforge = require('./src/config.js')
 
-const getAddonPage = () => {
+function getAddonHomePage () {
     return new Promise((resolve, reject) => {
         fs.stat(path.resolve(__dirname, './temp/addonHomePage.html'), (err, stats) => {
             if (err || !stats.isFile() || (new Date() - stats.mtime > 86400000)) {
-                request('https://www.curseforge.com/wow/addons', (err, res) => {
+                request(Curseforge.url.home, (err, res) => {
                     if (err) {
                         return reject(console.error(err))
                     }
@@ -41,10 +42,49 @@ const getAddonPage = () => {
         })
     })
 }
-getAddonPage().then(val => {
-    const $homepage = cheerio.load(val)
-    let addonList = $homepage('li.project-list-item').find('img.aspect__fill')
-    addonList.each((index, item) => {
-        console.log(item.attribs.alt)
+
+async function getPageAddonList(page) {
+    let list = []
+    await page.then(val => {
+        let $page = cheerio.load(val)
+        let addonList = $page('li.project-list-item').find('img.aspect__fill')
+        addonList.each((index, item) => {
+            list.push(item.attribs.alt)
+        })
     })
+    return list
+}
+
+async function getAddonCatagories(homepage) {
+    let list = []
+    await homepage.then(val => {
+        let $homepage = cheerio.load(val)
+        let addonCatagories = $homepage('li.tier-holder>ul.categories-tier.indent-0>li.category__item.pd-x-1')
+        addonCatagories.each((index, item) => {
+            let category = {}
+            let $item = $homepage(item)
+            category.title = $item.find('p.category__title').text()
+            category.url = $item.find('a').attr('href')
+            let subs = []
+            if ($item.next() && $item.next().attr('class').indexOf('tier-holder') > -1) {
+                $subList = $item.next()
+                $subList.children().each((index, item) => {
+                    let subCategory = {}
+                    $subItem = $homepage(item)
+                    subCategory.title = $subItem.find('p.category__title').text()
+                    subCategory.url = $subItem.find('a').attr('href')
+                    subs.push(subCategory)
+                })
+            }
+            console.log(subs)
+            if (subs) category.subCategories = subs
+            list.push(category)
+        })
+    })
+    return list
+}
+
+getAddonCatagories(getAddonHomePage()).then(val => {
+    console.log(val)
 })
+
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const baseUrl = 'https://www.curseforge.com'
+
+const CurseForge = {
+    urls: {
+        base: baseUrl,
+        home: baseUrl + '/wow/addons',
+    }
+}
+
+module.exports = CurseForge
