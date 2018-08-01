1b035ca73a544b15c2fccb839edf6a4ccde88498
NickWang <brotherstyx@gmail.com>
2018-08-01 11:40:05 +0800

    read addon toc file to js object

--- src/addonTocReader.js
+++ src/addonTocReader.js
@@ -1,19 +1,39 @@
 function addonTocReader(addonStr) {
     let index = 0
+    let lines = []
+    let toc = {}
+    toc.files = []
     while (index < addonStr.length) {
-        index += readline(addonStr, index)
+        let lineinfo = readline(addonStr, index)
+        index += lineinfo.length
+        lines.push(lineinfo.line)
     }
-    return "done"
+    for (let i in lines) {
+        if (lines[i].length <= 1) continue
+        if (lines[i][0] == "#" && lines[i][1] == "#") {
+            let propertyName = lines[i].split(':')[0].slice(3)
+            let propertyValue = lines[i].split(':')[1]
+            if (propertyValue[0] == " ") propertyValue = propertyValue.slice(1)
+            toc[propertyName] = propertyValue
+        } else {
+            toc.files.push(lines[i])
+        }
+    }
+    return toc
 }
 
 function readline(str, index) {
     let line = ''
-    while(str[index] != '\n') {
-        line += str[index]
-        index ++
+    let lineLength = 0
+    while(str[index + lineLength] != '\n') {
+        line += str[index + lineLength]
+        lineLength ++
     }
-    console.log(line)
-    return index
+    lineLength ++
+    return {
+        length: lineLength,
+        line: line,
+    }
 }
 
 
