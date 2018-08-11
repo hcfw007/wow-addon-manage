864e9af882ca956dbd7eef73bac9648d521c4827
NickWang <hcfw007@126.com>
2018-08-11 15:47:52 +0800

    trying to return objects for addon udpate info

--- src/checkAddonUpdates.js
+++ src/checkAddonUpdates.js
@@ -4,18 +4,45 @@ const cheerio = require('cheerio')
 const matchAddon = require('./matchAddonCursePage.js')
 const getLocalAddonList = require('./getLocalAddonList.js')
 
-getLocalAddonList().then(addonObjects => {
-    addonObjects.forEach(addon => {
-        matchAddon(addon.title).then(addonPage => {
-            $download = cheerio.load(addonPage)
-            let fileList = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a')
-            let latestVersion = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('td.project-file__name').attr('title')
-            let downloadURL = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('a.button.button--download.download-button.mg-r-05').attr('href')
-            if (latestVersion && downloadURL) {
-                console.log(addon.title + 'v' + latestVersion + ': ' + downloadURL)
-            } else {
-                console.log('cannot find page for ' + addon.title)
-            }
+
+function checkAddonUpdates() {
+    return new Promise((resolve, reject) => {
+        let addonUpdateInfoList = []
+        let matchPromiseList = []
+        getLocalAddonList().then(addonObjects => {
+            addonObjects.forEach(addon => {
+                let matchPromise = matchAddon(addon.title).then(addonPage => {
+                    $download = cheerio.load(addonPage)
+                    let fileList = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a')
+                    let latestVersion = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('td.project-file__name').attr('title')
+                    let downloadURL = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('a.button.button--download.download-button.mg-r-05').attr('href')
+                    let gameVersion = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('span.table__content.version__label').text()
+                    let uploadTimeStamp = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('abbr.tip.stardard-date.standard-datetime').attr('data-epoch')
+                    if (latestVersion && downloadURL) {
+                        //console.log(addon.title + 'v' + latestVersion + ': ' + downloadURL)
+                        let addonUpdateInfo = {}
+                        addonUpdateInfo.latestVersion = latestVersion
+                        addonUpdateInfo.gameVersion = gameVersion
+                        addonUpdateInfo.uploadTimeStamp = parseInt(uploadTimeStamp) * 1000
+                        addonUpdateInfo.downloadURL = downloadURL
+                        addonUpdateInfoList.push(addonUpdateInfo)
+                    } else {
+                        //console.log('cannot find page for ' + addon.title)
+                    }
+                    
+                })
+                matchPromiseList.push(matchPromise)
+            })
+            Promise.all(matchPromiseList).then(res => {
+                console.log(matchPromiseList)
+                resolve(addonUpdateInfoList)
+            }).catch(err => {
+                console.log('err')
+            })
         })
     })
+}
+
+checkAddonUpdates().then(val => {
+    console.log(val)
 })
