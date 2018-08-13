f16466038b0ae896bd50dbcd36fc9be810a107a0
NickWang <hcfw007@126.com>
2018-08-13 12:32:52 +0800

    work on windows so far

--- src/download.js
+++ src/download.js
@@ -9,36 +9,28 @@ const url2 = 'https://media.forgecdn.net/files/2597/855/Bagnon_8.0.2.zip'
 
 const request = require('request')
 
-function downloadAndFollowRedirect(url) {
-    request.get({
-        url: url,
-        followRedirect: false,
-    }, (err, res) => {
-        if (res.statusCode < 300) {
-            console.log('trying to download ' + url)
-            download(url , path.resolve('../temp/')).then(() => {
-                console.log('download success')
-            }).catch(err => {
-                console.log(err)
-            })
-        } else {
-            if (res.headers.location) {
-                console.log('redirecting to ' + res.headers.location)
-                downloadAndFollowRedirect(res.headers.location)
-            }
-        }
-    })
-}
+// function downloadAndFollowRedirect(url) {
+//     request.get({
+//         url: url,
+//         followRedirect: false,
+//     }, (err, res) => {
+//         if (res.statusCode < 300) {
+//             console.log('trying to download ' + url)
+//             download(url , path.resolve('../temp/')).then(() => {
+//                 console.log('download success')
+//             }).catch(err => {
+//                 console.log(err)
+//             })
+//         } else {
+//             if (res.headers.location) {
+//                 console.log('redirecting to ' + res.headers.location)
+//                 downloadAndFollowRedirect(res.headers.location)
+//             }
+//         }
+//     })
+// }
 
-request.get({
-    url: url2,
-    headers: {
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-    },
-}, (err, res) => {
-    console.log(err, res)
-}).pipe(fs.createWriteStream(path.resolve('./temp/download.zip')))
+request.get({url: url,}).pipe(fs.createWriteStream(path.resolve('./temp/download.zip')))
 // downloadAndFollowRedirect(url)
 
 // download(url2 , path.resolve('../temp/')).then(() => {
