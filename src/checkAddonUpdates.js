8e899fbdf0a943331c1f94d128e622ce5f43fb5f
NickWang <hcfw007@126.com>
2018-08-22 16:02:25 +0800

    fix typo & timestamp of addon

--- src/checkAddonUpdates.js
+++ src/checkAddonUpdates.js
@@ -20,7 +20,7 @@ function checkAddonUpdates() {
                     let latestVersion = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('td.project-file__name').attr('title')
                     let downloadURL = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('a.button.button--download.download-button.mg-r-05').attr('href')
                     let gameVersion = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('span.table__content.version__label').text()
-                    let uploadTimeStamp = $download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('abbr.tip.stardard-date.standard-datetime').attr('data-epoch')
+                    let uploadTimeStamp = parseInt($download('table.listing.listing-project-file.project-file-listing.b-table.b-table-a').find('tbody tr:first-child').find('abbr.tip.standard-date.standard-datetime').attr('data-epoch'))
                     if (latestVersion && downloadURL) {
                         //console.log(addon.title + 'v' + latestVersion + ': ' + downloadURL)
                         let addonUpdateInfo = {}
@@ -46,7 +46,7 @@ function checkAddonUpdates() {
     })
 }
 
-checkAddonUpdates()then(val => {
+checkAddonUpdates().then(val => {
     console.log(val)
 }).catch(err => {
     console.log(err)
