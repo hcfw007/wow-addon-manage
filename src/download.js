da3b64f59110f8d2765ded625d21c293ab4136bf
NickWang <hcfw007@126.com>
2018-09-03 15:18:56 +0800

    add timeout for download

--- src/download.js
+++ src/download.js
@@ -8,6 +8,7 @@ function download(url, filename = 'temp.zip') {
     return new Promise((resolve, reject) => {
         let file = path.resolve('./temp/' + filename)
         let writeStream = fs.createWriteStream(file)
+        console.log(appConfig.urls.base + url + '/file')
         request.get({url: appConfig.urls.base + url + '/file',}).pipe(writeStream) //It somehow works in a better network environment
         writeStream.on('finish', function() {
             resolve("success")
@@ -16,6 +17,9 @@ function download(url, filename = 'temp.zip') {
             console.log(err)
             reject("error")
         })
+        setTimeout(() => {
+            reject("time out")
+        }, 10000)
     })
     
 }
