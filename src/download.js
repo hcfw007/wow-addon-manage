d48bd116850e23fbff3999b0abdf3a8a2d1a8caf
NickWang <brotherstyx@gmail.com>
2018-09-07 22:49:38 +0800

    add existance check for temp folder

--- src/download.js
+++ src/download.js
@@ -6,7 +6,10 @@ const appConfig = require('./appConfig.js')
 
 function download(url, filename = 'temp.zip') {
     return new Promise((resolve, reject) => {
-        let file = path.resolve('./temp/' + filename)
+        if (fs.existsSync(path.resolve('./temp/'))) {
+            fs.mkdirSync(path.resolve('./temp/'))
+        }
+        let file = path.resolve('./temp/' + filename) //TODO temp dir may differ in package
         let writeStream = fs.createWriteStream(file)
         console.log(appConfig.urls.base + url + '/file')
         request.get({url: appConfig.urls.base + url + '/file',}).pipe(writeStream) //It somehow works in a better network environment
