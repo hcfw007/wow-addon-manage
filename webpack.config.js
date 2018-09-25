0e11f0fca389f5bdfe18b0c4407c3d9bb899b6e6
NickWang <hcfw007@126.com>
2018-09-25 15:46:10 +0800

    move bundle package location

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './ui/dist/'),
+    path: path.resolve(__dirname, './'),
     publicPath: '/',
   },
   module: {
