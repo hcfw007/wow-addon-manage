95e720a10a8bccca3856dcd7c3ab1f73801ad745
NickWang <hcfw007@126.com>
2018-09-28 15:37:52 +0800

    improve addon match method

--- src/matchAddonCursePage.js
+++ src/matchAddonCursePage.js
@@ -4,50 +4,62 @@ const appConfig = require('./appConfig.js')
 
 function matchAddon(title, matchedList) {
     return new Promise((resolve, reject) => {
+        title_upper_seperated = title.replace(/\S[A-Z][a-z]+?/g, function(word) {
+            return word[0] + " " + word.slice(1)
+        })
+        if (title_upper_seperated != title) {
+            let title_lower_seperated = title_upper_seperated.toLowerCase()
+            tryHyphen(title_lower_seperated)
+            tryInit(title_lower_seperated)
+        }
         title = title.toLowerCase()
         let no_match = true
-        title_hyphen = title.replace(/ /g, '-')
-        if (checkDuplicate(title_hyphen, matchedList)) {
-            matchedList.push(title_hyphen)
-            request.get(appConfig.urls.home + '/' + title_hyphen + '/files', (err, res) => {
-                if (err) {
-                    //console.log(err)
-                    return
-                }
-                let $page = cheerio.load(res.body.toString())
-                if ($page('header.h2.no-sub.no-nav>h2').text() == 'Not found') {
-                    //console.log('hyphen no match')
-                    return
-                }
-                //console.log('hyphen found')
-                no_match = false
-                resolve(res.body.toString())
-                //return
-            })
+        tryHyphen(title)
+        tryInit(title)
+        
+        function tryHyphen(t) {
+            let _t = t.replace(/ /g, '-')
+            if (checkDuplicate(_t, matchedList)) {
+                matchedList.push(_t)
+                request.get(appConfig.urls.home + '/' + _t + '/files', (err, res) => {
+                    if (err) {
+                        //console.log(err)
+                        return
+                    }
+                    let $page = cheerio.load(res.body.toString())
+                    if ($page('header.h2.no-sub.no-nav>h2').text() == 'Not found') {
+                        //console.log('hyphen no match')
+                        return
+                    }
+                    //console.log('hyphen found')
+                    no_match = false
+                    resolve(res.body.toString())
+                    //return
+                })
+            }
+        }
+        function tryInit(t) {
+            let _t = t.split(' ')[0]
+            if (checkDuplicate(_t, matchedList)) {
+                matchedList.push(_t)
+                request.get(appConfig.urls.home + '/' + _t + '/files', (err, res) => {
+                    if (err) {
+                        //console.log(err)
+                        return
+                    }
+                    let $page = cheerio.load(res.body.toString())
+                    if ($page('header.h2.no-sub.no-nav>h2').text() == 'Not found') {
+                        //console.log('init no match')
+                        return
+                    }
+                    //console.log('init found')
+                    no_match = false
+                    resolve(res.body.toString())
+                    //return
+                })
+            }
         }
         
-        title_init = title.split(' ')[0]
-
-        
-        if (checkDuplicate(title_init, matchedList)) {
-            matchedList.push(title_init)
-            request.get(appConfig.urls.home + '/' + title_init + '/files', (err, res) => {
-                if (err) {
-                    //console.log(err)
-                    return
-                }
-                let $page = cheerio.load(res.body.toString())
-                if ($page('header.h2.no-sub.no-nav>h2').text() == 'Not found') {
-                    //console.log('init no match')
-                    return
-                }
-                //console.log('init found')
-                no_match = false
-                resolve(res.body.toString())
-                //return
-            })
-        }
-        //TODO: use better method to decide matched or not
         setTimeout(() => {
             if (no_match) {
                 resolve('no match')
