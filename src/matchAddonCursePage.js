a032e6dfc1d2bc67d27541e1eb38b6c25a09abb3
NickWang <hcfw007@126.com>
2018-09-30 14:48:39 +0800

    add new match method (abbr) and fix no_match scope problem

--- src/matchAddonCursePage.js
+++ src/matchAddonCursePage.js
@@ -4,6 +4,7 @@ const appConfig = require('./appConfig.js')
 
 function matchAddon(title, matchedList) {
     return new Promise((resolve, reject) => {
+        let no_match = true
         title_upper_seperated = title.replace(/\S[A-Z][a-z]+?/g, function(word) {
             return word[0] + " " + word.slice(1)
         })
@@ -11,11 +12,12 @@ function matchAddon(title, matchedList) {
             let title_lower_seperated = title_upper_seperated.toLowerCase()
             tryHyphen(title_lower_seperated)
             tryInit(title_lower_seperated)
+            tryAbbr(title_lower_seperated)
         }
         title = title.toLowerCase()
-        let no_match = true
         tryHyphen(title)
         tryInit(title)
+        tryAbbr(title)
         
         function tryHyphen(t) {
             let _t = t.replace(/ /g, '-')
@@ -40,6 +42,30 @@ function matchAddon(title, matchedList) {
         }
         function tryInit(t) {
             let _t = t.split(' ')[0]
+            if (checkDuplicate(_t, matchedList)) {
+                matchedList.push(_t)
+                request.get(appConfig.urls.home + '/' + _t + '/files', (err, res) => {
+                    if (err) {
+                        //console.log(err)
+                        return
+                    }
+                    let $page = cheerio.load(res.body.toString())
+                    if ($page('header.h2.no-sub.no-nav>h2').text() == 'Not found') {
+                        //console.log('init no match')
+                        return
+                    }
+                    //console.log('init found')
+                    no_match = false
+                    resolve(res.body.toString())
+                    //return
+                })
+            }
+        }
+        function tryAbbr(t) {
+            let _t = ''
+            t.match(/\b[a-zA-z]/g).forEach((letter) => {
+                _t += letter
+            })
             if (checkDuplicate(_t, matchedList)) {
                 matchedList.push(_t)
                 request.get(appConfig.urls.home + '/' + _t + '/files', (err, res) => {
